987f3c7a95de5e4a7cae6e8f03311bdf18e3d03f
Mijin Kim <ttwsuf@naver.com>
2022-05-30 21:44:53 +0900

    init

--- src/BOJ/queue2/Main.java
+++ src/BOJ/queue2/Main.java
@@ -11,64 +11,65 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         Deque<Integer> q = new LinkedList<>();
 
 
-
-        int n = Integer.parseInt(br.readLine());
+        int N= Integer.parseInt(br.readLine());
 
         StringTokenizer st;
 
-        while( n-->0){
-            st = new StringTokenizer(br.readLine()," ");
-            switch (st.nextToken()){
+        while (N--> 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            switch (st.nextToken()) {
+
                 case "push":
                     q.offer(Integer.parseInt(st.nextToken()));
                     break;
 
                 case "pop":
                     Integer item = q.poll();
-                    if(item == null){
+                    if (item == null) {
                         sb.append(-1).append('\n');
-                    }
-                    else{
+                    } else {
                         sb.append(item).append('\n');
                     }
                     break;
-                case"size":
-                    sb.append(1).append('\n');
+
+                case "size":
+                    sb.append(q.size()).append('\n');
                     break;
 
                 case "empty":
-                    if(q.isEmpty()){
+                    if (q.isEmpty()) {
                         sb.append(1).append('\n');
-                    }else{
+                    } else {
                         sb.append(0).append('\n');
                     }
                     break;
                 case "front":
                     Integer ite = q.peek();
-                    if(ite == null){
+                    if (ite == null) {
                         sb.append(-1).append('\n');
-                    }else{
+                    } else {
                         sb.append(ite).append('\n');
                     }
                     break;
+
                 case "back":
                     Integer it = q.peekLast();
-                    if(it ==null){
+                    if (it == null) {
                         sb.append(-1).append('\n');
-                    } else{
+                    } else {
                         sb.append(it).append('\n');
                     }
                     break;
-
             }
         }
-                System.out.println(sb);
+        System.out.println(sb);
     }
 }
