cf2e7253b2172de8132d1021871e47dd4f880804
Mijin Kim <ttwsuf@naver.com>
2022-06-16 23:59:04 +0900

    init

---
+++ src/JAVA/Pra1.java
@@ -0,0 +1,23 @@
+package JAVA;
+
+
+
+
+public class Pra1 {
+    public static void  main(String[] args){
+        int[][] arr = {{2,3},{2,3,4},{2,3,4,5},{2,3,4,5,6,}};
+        for(int[] arrs : arr){
+            for(int arrss : arrs){
+                System.out.print(arrss);
+                System.out.print(",");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.print(sum(10,1));
+
+    }
+    public static int sum(int x, int y){
+        return x+y;
+    }
+}
